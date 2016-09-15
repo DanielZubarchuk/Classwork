09cb2bef1ee6def0f0fbb64a021ca90b2f5d8a0b
Student 8 <Student 8@10.8.33.171>
2016-09-15 13:49:33 -0400

    Jillian is in school.

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -4,7 +4,8 @@ public class ProceduralExample {
   public static void main(String[] args){
     System.out.println("Hello,world");
     String[]names = {"Jillian","Joseph","Jordan"};
-    System.out.println("Hello,my name is "+names[0]+".");
+    String[]years = {"Freshie","Sophomore","Junior","Senior"};
+    System.out.println("Hello,my name is "+names[0]+", and I am a "+years[3]+".");
   }
 }
 
