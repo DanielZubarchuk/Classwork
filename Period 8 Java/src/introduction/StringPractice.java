ec7a561e591b2998e3d970b247a2e1ac9bbccd88
Student 8 <Student 8@10.8.33.171>
2016-09-23 14:02:58 -0400

    StringsNew

--- Period 8 Java/src/introduction/StringPractice.java
+++ Period 8 Java/src/introduction/StringPractice.java
@@ -59,38 +59,42 @@ public class StringPractice {
 		
 	}
 	
-	public static void print(String s)
-	{
-		String printString = "";
-		int cutoff = 10;
-		//check for words to add
-		//IOW s had a length > 0 
-		while(s.length() > 0){
-			String cut = "";
-			String nextWord = "";
-			//check to see if the next word will fit on the line
-			//there must be still words to add
-			while(cut.length() + nextWord.length() < cutoff && s.length() > 0){
-				// add the next word to the line
-				cut += nextWord;
-				
-				s = s.substring(nextWord.length());
-				//identify the following word without the space
-				int endOfWord = s.indexOf(" ");
-				//if there are no more spaces, this is the last word
-				if(endOfWord == -1){
-					endOfWord = s.length() - 1;
-				}
-				
-				nextWord = s.substring(0, endOfWord);
-			}
-			
-			printString += cut+"\n";
-			
-		}
-		System.out.println(printString);
+	public static void print(String s){
+		 String printString = "";
+		 int cutoff = 25;
+		 //this while loop last as long as there are words left in the original String
+		 while(s.length() > 0){
+
+			 String currentCut = "";
+			 String nextWord = "";
+	
+			 //while the current cut is still less than the line length 
+			 //AND there are still words left to add
+			 while(currentCut.length()+nextWord.length() < cutoff && s.length() > 0){
+	
+				 //add the next word
+				 currentCut += nextWord;
+				 
+				 //remove the word that was added from the original String
+				 s = s.substring(nextWord.length());
+				 
+				 //identify the following word, exclude the space
+				 int endOfWord = s.indexOf(" ");
 		
-	}
+				 //if there are no more spaces, this is the last word, so add the whole thing
+				 if(endOfWord == -1) {
+					 endOfWord = s.length()-1;//subtract 1 because index of last letter is one les than length
+				 }
+	
+				 //the next word should include the space
+				 nextWord = s.substring(0,endOfWord+1);
+			 }
+	
+			 	printString +=currentCut+"\n";
+	
+			 }
+			 	System.out.print(printString);
+	 }
 	
 
 
