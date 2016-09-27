a49c3a68304cea8049f11be4cebed93df2ad0e5d
Student 8 <Student 8@10.8.33.171>
2016-09-27 13:58:27 -0400

    DanielMan

--- Period 8 Java/src/chatbot/DanielMain.java
+++ Period 8 Java/src/chatbot/DanielMain.java
@@ -46,25 +46,60 @@ public class DanielMain {
 		searchString = searchString.trim();
 		searchString = searchString.toLowerCase();
 		keyword = keyword.toLowerCase();
+		System.out.println("The Phrase is "+searchString);
+		System.out.println("The keyword is "+keyword);
 		int psn = searchString.indexOf(0);
+		System.out.println("The keyword was found at "+psn);
 		
 		while(psn >= 0){
 			String before = " ";
 			String after = " ";
 			if(psn > 0){
 				before = searchString.substring(psn-1, psn);
+				System.out.println("The Character before is "+before);
 			}
 			if(psn+ keyword.length() < searchString.length()){
 				after = searchString.substring(psn + keyword.length(), psn + keyword.length() + 1);                                            
+				System.out.println("The Character after is "+after);
 			}
-			if(before.compareTo("a") < 0 && after.compareTo("a") < 0){
+			if(before.compareTo("a") < 0 && after.compareTo("a") < 0 && noNegations(searchString, psn)){
+				System.out.println("Found "+keyword+" at "+psn);
 				return psn;
 			}
 			else{
 				psn = searchString.indexOf(keyword,psn+1);
+				System.out.print("Did not find "+keyword+", checking position "+psn);
 			}
 		}
 		return -1;
+	}
+
+	/**
+	 * Annotations
+	 * READABLE
+	 * @param searchString
+	 * @param psn
+	 * @return true if there is no negation words in front of psn
+	 */
+	private static boolean noNegations(String searchString, int psn) {
+		if(psn - 3 >= 0  && searchString.substring(psn-3, psn).equals("no "))
+		{
+			return false;
+		}
+		if(psn - 4 >= 0  && searchString.substring(psn-4, psn).equals("not "))
+		{
+			return false;
+		}
+		if(psn - 6 >= 0  && searchString.substring(psn-6, psn).equals("never "))
+		{
+			return false;
+		}
+		if(psn - 4 >= 0  && searchString.substring(psn-4, psn).equals("n't "))
+		{
+			return false;
+		}
+		return true;
+		
 	}
 
 	public static String promptInput() {
