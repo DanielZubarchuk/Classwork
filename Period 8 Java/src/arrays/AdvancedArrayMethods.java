f747705284e4bcc1df7253abaca65a2d613ba2e3
Student 8 <Student 8@10.8.33.171>
2016-10-20 14:03:02 -0400

    Swap

---
+++ Period 8 Java/src/arrays/AdvancedArrayMethods.java
@@ -0,0 +1,47 @@
+package arrays;
+
+public class AdvancedArrayMethods {
+	
+	static int[] array;
+	
+	public static void main(String[] args) {
+		String[] array = {"a", "b", "c", "d", "e", "f", "g", "h", "i"};
+		swap(array, 0, array.length - 1);
+	}
+	
+	private static void swap(String[] arr, int a, int b) {
+		String placeholder = arr[b];
+		arr[b] = arr[a];
+		arr[a] = placeholder;
+		
+	}
+
+	public static void arrayCopying(){
+		array = new int[100];
+		int[] someArray = new int[50];
+		someArray = array;
+		System.out.println(someArray.length);
+		someArray[30] = 1;
+		System.out.println(array[30]);
+		
+		methodA(someArray);
+		
+	}
+
+	private static void methodA(int[] someArray) {
+		int[] newArray = new int[someArray.length];
+		
+		
+	}
+	
+	public static void copyArray(int[] original, int[] target){
+		if(original.length == target.length){
+			for(int i = 0; i < original.length; i++){
+				target[i] = original[i];
+			}
+		}else{
+			System.out.println("ERROR, TRIED TO COPY ARRAYS OF TWO DIFF ARRAYS!");
+		}
+	}
+
+}
