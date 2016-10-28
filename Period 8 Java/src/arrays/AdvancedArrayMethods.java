ac11aa9805840e0c8e8e03b08970755e86b5845a
Student 8 <Student 8@10.8.33.171>
2016-10-28 13:32:34 -0400

    Prime

--- Period 8 Java/src/arrays/AdvancedArrayMethods.java
+++ Period 8 Java/src/arrays/AdvancedArrayMethods.java
@@ -8,8 +8,38 @@ public class AdvancedArrayMethods {
 		String[] array = {"a", "b", "c", "d", "e", "f", "g", "h", "i"};
 		//swap(array, 0, array.length - 1);
 		shuffle(array);
+		
+		listPrimes(120);
 	}
 	
+	private static void listPrimes(int limit) {
+		int lastToCheck = (int)(Math.sqrt(limit));
+		boolean[] numbers = new boolean[limit + 1];
+		for(int i = 0; i < limit + 1; i++){
+			numbers[i] = true;
+			
+		}
+		
+		numbers[0] = false;
+		numbers[1] = false;
+		for(int prime = 2; prime <= lastToCheck; prime++){
+			if(numbers[prime]){
+				System.out.println("\n"+prime+" is prime. Crossing off:");
+				for(int i = (int)(Math.pow(prime, 2)); i < limit+1; i += prime){
+					System.out.print(i + ", ");
+					numbers[i] = false;
+				}
+			}
+		}
+		System.out.println("The Primes are:");
+		for(int index = 0; index < numbers.length; index++){
+			if(numbers[index]){
+				System.out.print(index + ", ");
+			}
+		}
+		
+	}
+
 	private static void shuffle(Object[] array) {
 		for(int i = 0; i < array.length; i++){
 			int random = (int)(Math.random()*6);
