adc2c18bf3fbf05371d58de6a094b1425efceeea
Student 8 <Student 8@10.8.33.171>
2016-11-03 14:02:23 -0400

    ?

--- Period 8 Java/src/arrays/TwoDArraysIntro.java
+++ Period 8 Java/src/arrays/TwoDArraysIntro.java
@@ -1,10 +1,113 @@
 package arrays;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class TwoDArraysIntro {
-
+	
+	public static Scanner in = new Scanner(System.in);
+	static String[][] arr2D;
+	static String[][] pic;
+	static int starti;
+	static int startj;
+	static int treasurei;
+	static int treasurej;
+	
+	
 	public static void main(String[] args) {
+		arr2D = new String[5][5];
+		pic = new String[5][5];
+		for(int row = 0; row < arr2D.length; row++){
+			for(int col = 0; col < arr2D[row].length; col++){
+				arr2D[row][col] = "("+row+", "+col+")";
+			}
+		}
+		
+		starti = 2;
+		startj = 2;
+		treasurei = 4;
+		treasurej = 3;
+		
+		startExploring();
+	}
+	
+	private static void startExploring() {
+		while(true){
+			printPic(pic);
+			System.out.println("You are in room" +arr2D[starti][startj]+".");
+			
+			if(starti == treasurei && startj == treasurej){
+				break;
+			}
+			
+			System.out.println("What do you want to do?");
+			String input = in.nextLine();
+			
+			int[] newCoordinates = interpretInput(input);
+			starti = newCoordinates[0];
+			startj = newCoordinates[1];
+		}
+		
+	}
+
+	private static int[] interpretInput(String input) {
+		while(!isValid(input)){
+			System.out.println("Sorry, in this game, you can only use the WASD controls.");
+			System.out.println("Tell me again what you would like to do.");
+			input = in.nextLine();
+		}
+			
+			int currenti = starti;
+			int currentj = startj;
+			input = input.toLowerCase();
+			
+			if(input.equals("w")){
+				currenti --;
+			}
+			if(input.equals("s")){
+				currenti ++;
+			}
+			if(input.equals("a")){
+				currentj --;
+			}
+			if(input.equals("d")){
+				currentj ++;
+			}
+			
+			int[] newCoordinates = {starti, startj}; 
+			
+			if(currenti >= 0 && currenti < arr2D.length && currentj >= 0 && currentj < arr2D[0].length){
+				newCoordinates[0] = currenti;
+				newCoordinates[1] = currentj;
+				
+			}else{
+				System.out.println("You have reached the end of the universe and you cant go any further in that direction.");
+			}
+		return newCoordinates;
+	}		
+		
+	
+
+	private static boolean isValid(String input) {
+		String[] validKeys = {"w", "a", "s", "d"};
+		for(String key: validKeys){
+			if(input.toLowerCase().equals(key)){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static void makeGrid(){
+		String[][] field = new String [5][5];
+		for(int row = 0; row < field.length; row++){
+			for(int col = 0; col < field[row].length; col++){
+				field[row][col] = " ";
+			}
+		}
+	}
+	
+	public static void playMinesweeper(){
 		boolean[][] mines = new boolean[6][6];
 		plantMines(mines);
 		String[][] field = createField(mines);
@@ -26,13 +129,39 @@ public class TwoDArraysIntro {
 	}
 
 	private static String countNearby(boolean[][] mines, int row, int col) {
-		for(int r = row - 1; r <= row + 1; r++){
-			for(int c = col - 1; c <= col + 1; c++){
-				if(r >= 0 && r < mines.length && c >= 0 && c < mines[0].length){
-					
-				}
-			}
+//		for(int r = row - 1; r <= row + 1; r++){
+//			for(int c = col - 1; c <= col + 1; c++){
+//				if(r >= 0 && r < mines.length && c >= 0 && c < mines[0].length){
+//					
+//				}
+//			}
+//		}
+		
+//		int count = 0;
+//		for(int r = 0; r < mines.length; r++){
+//			for(int c = 0; c < mines[r].length; c++){
+//				if(Math.abs(r-row)+Math.abs(c-col) == 1 && mines[r][c]){
+//					count++;
+//				}
+//			}
+//		}
+//		
+//		return ""+count;
+		
+		int count = 0;
+		count += isValidAndTrue(mines, row-1, col);
+		count += isValidAndTrue(mines, row+1, col);
+		count += isValidAndTrue(mines, row, col-1);
+		count += isValidAndTrue(mines, row, col+1);
+		return ""+count;
+		
+	}
+
+	private static int isValidAndTrue(boolean[][] mines, int i, int j) {
+		if(i >= 0 && i < mines.length && j >= 0 && j < mines[0].length && mines[i][j]){
+			return 1;
 		}
+		return 0;
 	}
 
 	private static void plantMines(boolean[][] mines) {
