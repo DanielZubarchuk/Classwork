e62fb211fda7b8a0d77cdd57c8a21d1a77c4c559
Student 8 <Student 8@10.8.33.171>
2016-12-07 14:02:45 -0500

    ....

---
+++ Period 8 Java/src/guiPractice8/GUIApplication.java
@@ -0,0 +1,40 @@
+package guiPractice8;
+
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+
+public class GUIApplication extends JFrame{
+
+	private Screen currentScreen;
+	
+	
+	//main method for practice only
+	public static void main(String[] args){
+		new GUIApplication();
+		
+
+	}
+
+	public GUIApplication(){
+		// terminate program when window is closed
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		int x = 40; 
+		int y = 40;
+		int width = 600;
+		int height = 400;
+		setBounds(x,y,width,height);
+		initScreen();
+		setVisible(true);
+	}
+
+	private void initScreen() {
+		Screen startScreen = new Screen(getWidth(), getHeight());
+		currentScreen = startScreen;
+		
+	}
+	
+	public void paint(Graphics g){
+		g.drawImage(currentScreen.getImage(), 0, 0, null);
+	}
+}
