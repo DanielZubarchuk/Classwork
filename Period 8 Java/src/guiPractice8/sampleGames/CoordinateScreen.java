3121360a68ece7904320201c5a04efa8d7431c2e
Student 8 <Student 8@10.8.33.171>
2016-12-14 14:04:22 -0500

    ...

--- Period 8 Java/src/guiPractice8/sampleGames/CoordinateScreen.java
+++ Period 8 Java/src/guiPractice8/sampleGames/CoordinateScreen.java
@@ -1,10 +1,13 @@
 package guiPractice8.sampleGames;
 
+import java.awt.Color;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 
 import guiPractice8.Screen;
+import guiPractice8.component.Action;
+import guiPractice8.component.Button;
 import guiPractice8.component.TextArea;
 import guiPractice8.component.TextLabel;
 import guiPractice8.component.Visible;
@@ -13,6 +16,7 @@ public class CoordinateScreen extends Screen implements MouseMotionListener{
 
 	private TextLabel label;
 	private TextArea paragraph;
+	private Button button;
 	
 	public CoordinateScreen(int width, int height) {
 		super(width, height);
@@ -25,8 +29,15 @@ public class CoordinateScreen extends Screen implements MouseMotionListener{
 				"This is a whole paragraph. Notice how "
 				+ "as the paragraph gets to the edge"
 				+ " of the page, a new line is created.");
+		button = new Button(40,50,100,30,"Button",new Color(0,76,153), new Action(){
+			public void Act() {
+				// TODO Auto-generated method stub
+				
+			}
+			});
 		viewObjects.add(label);
 		viewObjects.add(paragraph);
+		viewObjects.add(button);
 		
 	}
 
