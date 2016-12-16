1595785c60ca96de01aff284a41424b2f85e2a90
Student 8 <Student 8@10.8.33.171>
2016-12-16 13:59:50 -0500

    ....

--- Period 8 Java/src/guiPractice8/component/Graphic.java
+++ Period 8 Java/src/guiPractice8/component/Graphic.java
@@ -1,6 +1,9 @@
 package guiPractice8.component;
 
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+
+import javax.swing.ImageIcon;
 
 public class Graphic implements Visible {
 
@@ -14,9 +17,31 @@ public class Graphic implements Visible {
 		this.x = x;
 		this.y = y;
 		loadedImages = false;
-		loadImages(imageLocation, 0.0);
+		loadImages(imageLocation, 0, 0);
 	}
 	
+	private void loadImages(String imageLocation, int w, int h) {
+		try{
+			ImageIcon icon = new ImageIcon(imageLocation);
+			
+			if(w == 0 && h == 0){
+				image = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
+				
+				Graphics2D g = image.createGraphics();
+				g.drawImage(icon.getImage(), 0, 0, null);
+			}else{
+				
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	private void loadImages(String imageLocation, double scale) {
+		
+		
+	}
+
 	@Override
 	public BufferedImage getImage() {
 		// TODO Auto-generated method stub
